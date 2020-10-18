124715caef3e993044354f61e19ad4bd5555fd59
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2020-10-18 16:53:04 +0300

    Update the CyclicRotation solution

--- Lesson-2/CyclicRotation/Solution.java
+++ Lesson-2/CyclicRotation/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
 
     public int[] solution(int[] A, int K) {
-        
+        int counter = A.length;
+        int[] B = new int[counter];
+
+        for (int i = 0; i < counter; i++) {
+            B[(i + K) % counter] = A[i];
+        }
+
+        return B;
     }
 }
